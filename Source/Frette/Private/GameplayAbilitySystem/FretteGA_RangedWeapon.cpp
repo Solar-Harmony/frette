237e5a290eb21a082c3d63dbc65ee1ab87d1360b
Yanick Duhaime <YanickDuhaime1@gmail.com>
2026-02-08 21:37:50 -0500

    feat: juste rendu callable dans les BP

--- Source/Frette/Private/GameplayAbilitySystem/FretteGA_RangedWeapon.cpp
+++ Source/Frette/Private/GameplayAbilitySystem/FretteGA_RangedWeapon.cpp
@@ -29,29 +29,6 @@ void UFretteGA_RangedWeapon::SpawnProjectile(const UFretteRangedWeaponInstance* 
 		);
 }
 
-void UFretteGA_RangedWeapon::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
-{
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-
-	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
-	{
-		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
-	}
-
-	//Devrait être fait dans lee blueprint, les arc vont devoir charger le tir quand on ActivateAbility 
-	//et gerer le spawn du projectile seulement quand le tir est relaché
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green,TEXT("Ranged Weapon Ability Activated"));
-	UFretteRangedWeaponInstance* WeaponInstance = Cast<UFretteRangedWeaponInstance>(GetSourceObject(Handle, ActorInfo));
-	check(WeaponInstance);
-
-	if (HasAuthority(&ActivationInfo))
-	{
-		SpawnProjectile(WeaponInstance);
-	}
-
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
-}
-
 bool UFretteGA_RangedWeapon::CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, FGameplayTagContainer* OptionalRelevantTags) const
 {
 	if (!Super::CheckCost(Handle, ActorInfo, OptionalRelevantTags))
--- Source/Frette/Private/GameplayAbilitySystem/FretteGA_RangedWeapon.h
+++ Source/Frette/Private/GameplayAbilitySystem/FretteGA_RangedWeapon.h
@@ -14,7 +14,9 @@ class UFretteGA_RangedWeapon : public UFretteGameplayAbility
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintCallable)
 	void SpawnProjectile(const UFretteRangedWeaponInstance* WeaponInstance);
+
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual bool CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
 	virtual void ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
