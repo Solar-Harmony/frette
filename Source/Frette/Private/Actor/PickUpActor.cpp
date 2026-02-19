e7e52774f6a3d2a50429aea691709cc852c45fba
William Boulanger <willbou1@hotmail.com>
2026-02-19 12:18:17 -0500

    Commentaires pour la PR

--- Source/Frette/Private/Actor/PickUpActor.cpp
+++ Source/Frette/Private/Actor/PickUpActor.cpp
@@ -1,47 +1,58 @@
-
-
-
 #include "Actor/PickUpActor.h"
-
 #include "Character/FrettePlayerCharacter.h"
+#include "PhysicsEngine/BodySetup.h"
 
 APickUpActor::APickUpActor()
 {
-	PrimaryActorTick.bCanEverTick = true;
-	
-	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
-	SetRootComponent(Root);
-	
+	PrimaryActorTick.bCanEverTick = false;
+
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
+	SetRootComponent(OverlapSphere);
+	OverlapSphere->SetSphereRadius((Item_Size / 2.f) * Sphere_Radius_Ratio);
+	OverlapSphere->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickUpActor::PickedUp);
+
 	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Item"));
-	StaticMesh->SetupAttachment(Root);
-	
-	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
-	Sphere->SetupAttachment(Root);
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &APickUpActor::PickedUp);
+	StaticMesh->SetupAttachment(OverlapSphere);
+	StaticMesh->SetSimulatePhysics(false);
+	StaticMesh->SetCollisionProfileName(TEXT("BlockAll"));
+	StaticMesh->SetEnableGravity(true);
+	StaticMesh->SetMassOverrideInKg(NAME_None, 10.0f, true);
 }
 
 void APickUpActor::OnConstruction(const FTransform& Transform)
 {
 	Super::OnConstruction(Transform);
 
-	if (ItemMesh)
+	if (IsValid(ItemMesh))
 	{
+		const UBodySetup* BodySetup = ItemMesh->GetBodySetup();
+		const bool bHasCollision = IsValid(BodySetup) && BodySetup->AggGeom.GetElementCount() > 0;
+		if (!bHasCollision)
+		{
+			UE_LOG(LogTemp, Error,
+				TEXT("PickUpActor '%s': ItemMesh '%s' has no collision data. Assign a mesh with collision."),
+				*GetName(), *ItemMesh->GetName());
+			return;
+		}
+
 		StaticMesh->SetStaticMesh(ItemMesh);
 		const FBoxSphereBounds Bounds = ItemMesh->GetBounds();
 		const FVector CurrentSize = Bounds.BoxExtent * 2.0f;
 		const float MaxDim = FMath::Max3(CurrentSize.X, CurrentSize.Y, CurrentSize.Z);
 		const float ScaleFactor = (MaxDim > 0.0f) ? (Item_Size / MaxDim) : 1.0f;
 		StaticMesh->SetRelativeScale3D(FVector(ScaleFactor));
-		const float SphereRadius = Item_Size / 2.f * 1.3f;
-		Sphere->SetSphereRadius(SphereRadius); // Make sure it's not too hard to pick it up
-		Sphere->SetRelativeLocation(FVector(0.f, 0.f, SphereRadius));
+		StaticMesh->SetRelativeLocation(FVector(0.f, 0.f, Item_Size / 2.f));
 	}
 }
 
 void APickUpActor::PickedUp(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
 							int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (!(OtherActor && OtherActor != this && OtherActor->IsA(AFrettePlayerCharacter::StaticClass())))
+	const bool bCanPickUp = IsValid(OtherActor)
+		&& OtherActor != this
+		&& OtherActor->IsA(AFrettePlayerCharacter::StaticClass());
+	if (!bCanPickUp)
 		return;
 	
 	OnPickedUp.Broadcast(OtherActor);
@@ -49,28 +60,3 @@ void APickUpActor::PickedUp(UPrimitiveComponent* OverlappedComp, AActor* OtherAc
 	if (bDestroyOnPickUp)
 		Destroy();
 }
-
-void APickUpActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-void APickUpActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-	YawAngle += DeltaTime * Item_Rotation_Speed;
-	YawAngle = FMath::Fmod(YawAngle, 360.f);
-	
-	ZPhase += DeltaTime * Item_Z_Mod_Speed;
-	ZPhase = FMath::Fmod(ZPhase, 2.f * PI);
-	
-	const FRotator Rotator = FRotator(0.f, YawAngle, 0.f);
-	Root->SetRelativeRotation(Rotator);
-	Root->SetRelativeLocation(FVector(
-		0.f, 
-		0.f,
-		Item_Z_Offset + (FMath::Sin(ZPhase) + 1.f) / 2.f * Item_Z_Mod_Range));
-}
-
--- Source/Frette/Public/Actor/PickUpActor.h
+++ Source/Frette/Public/Actor/PickUpActor.h
@@ -1,10 +1,10 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PostProcessSubsystem.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/Actor.h"
+#include "Interface/InteractibleInterface.h"
 #include "PickUpActor.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPickedUp, AActor*, InteractingActor);
@@ -15,7 +15,6 @@ class FRETTE_API APickUpActor : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	APickUpActor();
 	
 	UFUNCTION()
@@ -31,34 +30,20 @@ public:
 	
 	UPROPERTY()
 	UStaticMeshComponent* StaticMesh;
-	
+
 	UPROPERTY()
-	USceneComponent* Root;
-	
-	UPROPERTY()
-	USphereComponent* Sphere;
+	USphereComponent* OverlapSphere;
 	
 	UPROPERTY(BlueprintAssignable)
 	FOnPickedUp OnPickedUp;
 	
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-	
 	virtual void OnConstruction(const FTransform& Transform) override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	
 private:
 	
 	float YawAngle = 0.f;
-	float ZPhase = 0.f;
 	
 	const float Item_Size = 40.f /*cm*/;
-	const float Item_Rotation_Speed = (360.f /*degrees*/ / 4.f /*seconds*/);
-	const float Item_Z_Mod_Speed = (2.f * PI /*radians*/ / 6.f /*seconds*/);
-	const float Item_Z_Mod_Range = 10.f /*cm*/;
-	const float Item_Z_Offset = 5.f /*cm*/;
+	const float Sphere_Radius_Ratio = 1.5f;
 };
