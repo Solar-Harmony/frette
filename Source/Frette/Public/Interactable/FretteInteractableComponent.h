92006de2849e3ea8f33dd6c5d2ac71ff4dd4af8d
Yanick Duhaime <YanickDuhaime1@gmail.com>
2026-04-05 20:27:44 -0400

    feat: cache les messages d'intéractions quand la trap est pas triggered
    - peut pas interagire avec la trap quand pas triggerered
    - delete un fichier non utilisé
    - déplacer les fichier de la trap dans les GPI
    - Bug: le joueur peut sauter pendant qu'il est dans la trap (Va être fixed quand je vais faire le jump en gameplay ability)

--- Source/Frette/Public/Interactable/FretteInteractableComponent.h
+++ Source/Frette/Public/Interactable/FretteInteractableComponent.h
@@ -5,44 +5,48 @@
 #include "FretteInteractableComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnBeginHover);
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEndHover);
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteract, AFrettePlayerCharacter*, Instigator);
 
-UCLASS(ClassGroup=(Frette), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup=(Frette), meta=(BlueprintSpawnableComponent))
 class FRETTE_API UFretteInteractableComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
+	UFretteInteractableComponent();
 	virtual void BeginPlay() override;
-	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
 	UPROPERTY(BlueprintAssignable)
 	FOnBeginHover OnBeginHover;
-	
+
 	UPROPERTY(BlueprintAssignable)
 	FOnEndHover OnEndHover;
-	
+
 	UPROPERTY(BlueprintAssignable)
 	FOnInteract OnInteract;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
 	TObjectPtr<UStaticMeshComponent> Mesh;
-	
+
 	UPROPERTY(EditAnywhere, Category = "Interaction")
 	FText Message = FText::FromString("Press E to interact");
-	
+
 	UPROPERTY(EditAnywhere, Category = "Interaction")
 	float OutlineThickness = 2.f;
-	
+
 	UPROPERTY(EditAnywhere, Category = "Interaction")
 	float OutlineAlpha = 0.7f;
-	
+
 	UPROPERTY(EditAnywhere, Category = "Interaction")
 	FColor OutlineColor = FColor::White;
-	
-	UPROPERTY(EditAnywhere, Category = "Interaction")
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = "Interaction")
 	bool bShowOutline = false;
-	
-	UPROPERTY(EditAnywhere, Category = "Interaction")
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = "Interaction")
 	bool bShowMessage = true;
 };
