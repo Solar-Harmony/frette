15cc7afa44c23d5bed38924fcaa628a986a641b0
Yanick Duhaime <YanickDuhaime1@gmail.com>
2026-04-09 12:33:29 -0400

    feat: ajouter le anim blueprint pour le wolf

---
+++ Source/Frette/Private/Character/FretteEnemyCharacter.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Character/FretteEnemyCharacter.h"
+
---
+++ Source/Frette/Public/Character/FretteEnemyCharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/FretteBaseCharacter.h"
+#include "FretteEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRETTE_API AFretteEnemyCharacter : public AFretteBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
